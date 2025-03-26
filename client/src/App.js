59b4808b2da2806a19f4f2e9a27784099793411c
Akthar2850 <syedakram2850@gmail.com>
2025-03-27 01:09:06 +0530

    Fix: prevent undefined download link after upload

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { uploadFile } from './service/api';
 function App() {
   const [file, setFile] = useState('');
   const [result, setResult] = useState('');
-
   const fileInputRef = useRef();
 
   const url = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
@@ -18,24 +17,30 @@ function App() {
         data.append("file", file);
 
         const response = await uploadFile(data);
-        setResult(response.path);
+        console.log("Upload response:", response);
+
+        if (response?.path) {
+          setResult(response.path);
+        } else {
+          console.error("Upload failed or response.path is undefined:", response);
+        }
       }
-    }
+    };
     getImage();
-  }, [file])
+  }, [file]);
 
   const onUploadClick = () => {
     fileInputRef.current.click();
-  }
+  };
 
   return (
     <div className='container'>
-      <img src={url} className='img' />
+      <img src={url} className='img' alt="banner" />
       <div className='wrapper'>
         <h1>Simple file sharing!</h1>
         <p>Upload and share the download link.</p>
-        
-        <button onClick={() => onUploadClick()}>Upload</button>
+
+        <button onClick={onUploadClick}>Upload</button>
         <input
           type="file"
           ref={fileInputRef}
@@ -43,14 +48,14 @@ function App() {
           onChange={(e) => setFile(e.target.files[0])}
         />
 
-{result && (
-  <div className="result-box">
-    <p>Your download link:</p>
-    <input type="text" readOnly value={result} />
-    <button onClick={() => navigator.clipboard.writeText(result)}>Copy Link</button>
-    <a href={result} target='_blank' rel="noreferrer">Download</a>
-  </div>
-)} 
+        {result && (
+          <div className="result-box">
+            <p>Your download link:</p>
+            <input type="text" readOnly value={result} />
+            <button onClick={() => navigator.clipboard.writeText(result)}>Copy Link</button>
+            <a href={result} target='_blank' rel='noreferrer'>Download</a>
+          </div>
+        )}
       </div>
     </div>
   );
